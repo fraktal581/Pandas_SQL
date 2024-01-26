1b82aafa905408a758beda8de2e29422f6fda8e0
Stas Vorotyntsev <fraktal581@gmail.com>
2024-01-26 09:09:02 +0300

    Загрузчик SQL Server  проверяет наличие существования таблицы

--- SQL_writer/entry_sales_to_DB_MS_SQL.py
+++ SQL_writer/entry_sales_to_DB_MS_SQL.py
@@ -17,16 +17,14 @@ check_table_query = "SELECT 1 FROM sys.tables WHERE name = '" + table_name + "'"
 # Проверяем существование таблицы
 if cursor.execute(check_table_query).fetchone():
     print("Таблица", table_name, "уже существует")
-#else:
+else:
     # Формируем SQL-запрос для создания таблицы
     #  sql_query = 'CREATE TABLE ' + table + ' (' + ', '.join(columns) + ')'
-#    sql_query = f"""
-#               CREATE TABLE {data_base}.dbo.{table_name}
-#                (id INT, segment VARCHAR(255), access_group VARCHAR(255), main_meneger VARCHAR(255), reduction_factor VARCHAR(255), realisation VARCHAR(255), payment_schedule VARCHAR(255), client_name VARCHAR(255), main_organisation VARCHAR(255), type_of_nomenclature VARCHAR(255), nomenclature VARCHAR(255), vendor VARCHAR(255), date_period DATE, count INT, billings SMALLMONEY)"""
-
-#cursor.execute(sql_query)
-
-print(f'таблица dbo.{table_name} создана')
+    sql_query = f"""
+                CREATE TABLE {data_base}.dbo.{table_name}
+                (id INT, segment VARCHAR(255), access_group VARCHAR(255), main_meneger VARCHAR(255), reduction_factor VARCHAR(255), realisation VARCHAR(255), payment_schedule VARCHAR(255), client_name VARCHAR(255), main_organisation VARCHAR(255), type_of_nomenclature VARCHAR(255), nomenclature VARCHAR(255), vendor VARCHAR(255), date_period DATE, count INT, billings SMALLMONEY)"""
+    cursor.execute(sql_query)
+    print(f'таблица dbo.{table_name} создана')
 connection_DB.commit
 connection_DB.close
 
